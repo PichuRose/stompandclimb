5291d16ff8a406baee59b098d4f283973a34c8ad
PichuRose <79072676+PichuRose@users.noreply.github.com>
2025-04-27 19:21:05 -0500

    slightly updated hypercollider version to fix a bug with end gateway achievements and hookshots added recipe for echo shards reduced netherite cost of collar/ring by 7x moved forward chest carry because people couldn't see

--- src/main/java/pichurose/stompandclimb/commands/StompAndClimbCustomCarryCommand.java
+++ src/main/java/pichurose/stompandclimb/commands/StompAndClimbCustomCarryCommand.java
@@ -22,7 +22,7 @@ public class StompAndClimbCustomCarryCommand {
         {0,     0,      0},
         {0.5,   -1,     -0.5},
         {0.5,   -1,     0.5},
-        {0.2,   -1.1,   0},
+        {0.27,   -1.1,   0},
         {0.25,  -0.8,   0}
     };
 
